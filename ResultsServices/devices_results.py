17239d88858502db389d35501aff36553af26dbd
naveenreddysg <naveenreddy.sg@gmail.com>
2018-04-16 16:16:30 +0530

    commited for devices

---
+++ ResultsServices/devices_results.py
@@ -0,0 +1,32 @@
+from ModelServices.devices_service import DevicesService
+
+class DeviceResults:
+
+    def __init__(self, current_start_date, current_end_date, previous_start_date, previous_end_date):
+        self.current_start_date = current_start_date
+        self.current_end_date = current_end_date
+        self.previous_start_date = previous_start_date
+        self.previous_end_date = previous_end_date
+
+    def main(self):
+
+        current_results = DevicesService(self.current_start_date, self.current_end_date).get_data()
+        previous_results = DevicesService(self.previous_start_date, self.previous_end_date).get_data()
+
+        main_result = [
+            {
+            'Device': 'Desktop',
+            'Goal Completions': current_results[0]['desktop'],
+            'Previous': previous_results[0]['desktop'],
+            'Change': str(round(((float(current_results[0]['desktop'])-float(previous_results[0]['desktop'])) /
+                                 float(previous_results[0]['desktop'])) * 100, 2)) + '%'
+            },
+            {
+            'Device': 'Mobile + Tablet',
+            'Goal Completions': current_results[0]['mobileTablet'],
+            'Previous': previous_results[0]['mobileTablet'],
+            'Change': str(round(((float(current_results[0]['mobileTablet']) - float(previous_results[0]['mobileTablet'])) /
+                              float(previous_results[0]['mobileTablet'])) * 100, 2)) + '%'
+            }
+         ]
+        return main_result
--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from ResultsServices.sessions_results import SessionsResults
 from ResultsServices.sessions_category_result import SessionsCategoryResults
 from ResultsServices.events_results import EventsResults
+from ModelServices.devices_service import DevicesService
+from ResultsServices.devices_results import DeviceResults
 #=======================================================================================================================
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://webanalytics:PyPrince@123@68.178.217.13/webanalytics'
@@ -18,10 +20,12 @@ def index():
     sessions = SessionsResults('2017-10-01', '2017-10-31', '2017-12-1', '2017-12-30')
     session_category = SessionsCategoryResults('2017-10-01', '2017-10-31', '2017-12-1', '2017-12-30')
     events = EventsResults('2017-10-01', '2017-10-31', '2017-12-1', '2017-12-30')
+    devices = DeviceResults('2017-10-01', '2017-10-31', '2017-12-1', '2017-12-30')
     return render_template("results.html",
                            sessions=sessions.main(),
                            session_category=session_category.main(),
-                           events=events.main()
+                           events=events.main(),
+                           devices=devices.main()
                            )
 
 if __name__ == '__main__':
